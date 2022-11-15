df7499ba7e7fa25a279d366bb49c763c41bc75f2
Volodia <volodiasyd@gmail.com>
2022-11-15 14:40:47 +0200

    accelerometer change

--- app.js
+++ app.js
@@ -17,16 +17,7 @@ if (window.DeviceMotionEvent == undefined) {
    window.addEventListener('devicemotion', accelerometerUpdate, true);
 }
 
-function accelerometerUpdate(e) {
-   var aX = e.accelerationIncludingGravity.x * 1;
-   var aY = e.accelerationIncludingGravity.y * 1;
-   var aZ = e.accelerationIncludingGravity.z * 1;
-   //The following two lines are just to calculate a
-   // tilt. Not really needed.
-   xPosition = Math.atan2(aY, aZ);
-   yPosition = Math.atan2(aX, aZ);
-   card.style.transform = `rotateY(${xPosition}deg) rotateX(${yPosition}deg)`;
-}
+function accelerometerUpdate(e) {}
 
 //Moving Animation Event
 container.addEventListener('mousemove', (e) => {
---
+++ main.js
@@ -0,0 +1,56 @@
+(() => {
+   var balloon = document.querySelector('.card'),
+      // line = document.querySelector('.card'),
+      lastZ = 0,
+      lastY = 0,
+      minDiff = 2,
+      tm = null,
+      wip = 0,
+      wipDivizor = 12;
+
+   function handleOrientation(event) {
+      console.log(event.absolute);
+
+      var absolute = event.absolute;
+      var alpha = event.alpha;
+      var beta = event.beta;
+      var gamma = event.gamma;
+
+      if (Math.abs(lastZ - gamma) > minDiff) {
+         clearTimeout(tm);
+
+         // telling the body about the direction for the animation
+         //  if (gamma < 0) {
+         //     document.body.setAttribute('data-moving', 'right');
+         //     line.style.width = -gamma * 1.1 + 'px';
+         //     line.style.transform = 'translateX(' + gamma * 0.1 + 'px)';
+         //  }
+         //  if (gamma > 0) {
+         //     line.style.width = gamma * 0.5 + 'px';
+         //     document.body.setAttribute('data-moving', 'left');
+         //  }
+
+         // adding wipplash effect
+         wip = (gamma - lastZ) / wipDivizor;
+
+         // rotating the balloon
+         balloon.style.transform = 'rotateZ(' + -1 * (gamma + wip) + 'deg)';
+
+         tm = setTimeout((_) => {
+            // ending the wipplash effect
+            balloon.style.transform = 'rotateZ(' + -1 * (gamma - wip) + 'deg)';
+         }, 400);
+
+         // and now we store the gamma
+         lastZ = gamma;
+      }
+
+      if (Math.abs(lastY - beta) > minDiff) {
+         // gatting farther or closer
+         balloon.style.width = -beta + 200 + 'px';
+         lastY = beta;
+      }
+   }
+
+   window.addEventListener('deviceorientation', handleOrientation, true);
+})();
